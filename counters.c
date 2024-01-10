dc3d074fe825c55a47816bc09610e80409b3e6be
Laafer Issam <lissam@e1r3p9.1337.ma>
2024-01-10 19:01:01 +0100

    ficing some bugs

--- counters.c
+++ counters.c
@@ -6,7 +6,7 @@
 /*   By: lissam <lissam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 09:45:25 by lissam            #+#    #+#             */
-/*   Updated: 2024/01/10 17:51:00 by lissam           ###   ########.fr       */
+/*   Updated: 2024/01/10 18:58:40 by lissam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ void	moves_counter(t_vars *data)
 {
 	char	*moves;
 	char	*colletives;
+	char	*colletives_max;
 
 	moves = ft_itoa(data->count_moves);
 	colletives = ft_itoa(data->count_collectives);
+	colletives_max = ft_itoa(data->colletives);
 	ft_putstr("Moves: ");
 	ft_putstr(moves);
 	ft_putstr("\n");
@@ -27,6 +29,10 @@ void	moves_counter(t_vars *data)
 	mlx_string_put(data->mlx, data->mlx_win, 10, 30, 0x00FF0000,
 		"Number of bananas: ");
 	mlx_string_put(data->mlx, data->mlx_win, 200, 30, 0x00FF0000, colletives);
+	mlx_string_put(data->mlx, data->mlx_win, 220, 30, 0x00FF0000, "/");
+	mlx_string_put(data->mlx, data->mlx_win, 240, 30, 0x00FF0000,
+		colletives_max);
 	free(moves);
 	free(colletives);
+	free(colletives_max);
 }
