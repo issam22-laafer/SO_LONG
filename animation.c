d7c4acf6f46843826cf0af1fe96cb29058167c42
Laafer Issam <lissam@e1r3p9.1337.ma>
2024-01-11 17:31:22 +0100

    enemy move

--- animation.c
+++ animation.c
@@ -6,11 +6,83 @@
 /*   By: lissam <lissam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 09:12:56 by lissam            #+#    #+#             */
-/*   Updated: 2024/01/11 09:12:58 by lissam           ###   ########.fr       */
+/*   Updated: 2024/01/11 17:30:27 by lissam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+// void	go_left(t_vars *data)
+// {
+// 	int	i;
+// 	int	j;
+
+// 	i = 0;
+// 	while (i < data->map_height)
+// 	{
+// 		j = 0;
+// 		while (j < data->map_width)
+// 		{
+// 			if (data->map[i][j] == 'N')
+// 			{
+// 				if (data->map[i][j - 1] != '1' && data->map[i][j - 1] != 'C')
+// 				{
+// 					data->map[i][j] = '0';
+// 					data->map[i][j - 1] = 'N';
+// 					draw_floor(data);
+// 					draw_ennemie(data);
+// 				}
+// 			}
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// }
+// void	go_right(t_vars *data)
+// {
+// 	int	i;
+// 	int	j;
+
+// 	i = 0;
+// 	while (i < data->map_height)
+// 	{
+// 		j = 0;
+// 		while (j < data->map_width)
+// 		{
+// 			if (data->map[i][j] == 'N')
+// 			{
+// 				if (data->map[i][j + 1] != '1' && data->map[i][j + 1] != 'C')
+// 				{
+// 					data->map[i][j] = '0';
+// 					data->map[i][j + 1] = 'N';
+// 					draw_floor(data);
+// 					draw_ennemie(data);
+// 				}
+// 			}
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// }
+
+// void	move_enemie(t_vars *data)
+// {
+// 	static int	e;
+
+// 	if (e < 100)
+// 	{
+// 		go_left(data);
+// 	}
+// 	else if (e < 200)
+// 	{
+// 		go_right(data);
+// 		if (e + 1 == 200)
+// 		{
+// 			e = 0;
+// 		}
+// 	}
+// 	e += 1;
+// }
 
 void	check_ennemie_images(t_vars *data)
 {
@@ -52,9 +124,9 @@ void	render2(t_vars *data)
 	}
 }
 
-int	render_ennemie(t_vars *data)
+void	animate(t_vars *data)
 {
-	static int	frame = 0;
+	static int	frame;
 
 	check_ennemie_images(data);
 	if (frame < 20)
@@ -78,5 +150,11 @@ int	render_ennemie(t_vars *data)
 	}
 	render2(data);
 	frame += 1;
+}
+
+int	render_ennemie(t_vars *data)
+{
+	// move_enemie(data);
+	animate(data);
 	return (0);
 }
